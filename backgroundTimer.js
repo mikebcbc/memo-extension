13b9e4414ae2048a90bdff3b009d671527ec6fb3
mikebcbc <mikebconstantino@gmail.com>
2017-12-09 20:26:14 -0500

    Styles, login view, get AuthToken

--- backgroundTimer.js
+++ backgroundTimer.js
@@ -1,13 +1,6 @@
-var contentUrls = [];
+var contentArr = [];
 var currentTab = null;
 var currentContent = null;
-
-$.get("http://localhost:3001/contents", function(data) {
-  contentUrls = data.map(function(content) {
-    return content.link;
-  })
-});
-
 var timer = new Tock({
   interval: 1000,
   callback: function() {
@@ -15,16 +8,52 @@ var timer = new Tock({
   }
 });
 
+$.get("http://localhost:3001/contents", function(data) {
+  contentArr = data.map(function(content) {
+    return {url: content.link, id: content._id};
+  })
+});
+
 function sendCurrentTime(time) {
   chrome.runtime.sendMessage({type: "time", timeSpent: time});
 }
 
+function postContent(content) {
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:3001/users/content",
+    datatype: 'json',
+    data: JSON.stringify(content),
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJuYW1lIjoiZGVtbyIsImNvbnRlbnQiOlt7ImNvbnRlbnRJZCI6IjVhMjFmMWExYmYwOGU0MjI5NTFkNDc5ZiIsInRpbWUiOjQ1NSwiY29tcGxldGVkIjp0cnVlfSx7ImNvbnRlbnRJZCI6IjVhMjZkYmVjZTdlZWZlYjJjMmRkZmIxMSIsInRpbWUiOjIyMSwiY29tcGxldGVkIjp0cnVlfSx7ImNvbnRlbnRJZCI6IjVhMjVjMTQzNjE0MzkxNDNiYzRiYTU5MSIsInRpbWUiOjM0MCwiY29tcGxldGVkIjpmYWxzZX0seyJjb250ZW50SWQiOiI1YTIxZjMyYmJmMDhlNDIyOTUxZDQ3YTAiLCJ0aW1lIjo0MjIsImNvbXBsZXRlZCI6ZmFsc2V9LHsiY29udGVudElkIjoiNWEyNmRkYjNlN2VlZmViMmMyZGRmYjM5IiwidGltZSI6MTIwMiwiY29tcGxldGVkIjpmYWxzZX1dfSwiaWF0IjoxNTEyNzc5NjMwLCJleHAiOjE1MTI3ODMyMzAsInN1YiI6ImRlbW8ifQ.XNPHRC1nqNDRQxjTCzSTTR5kkMJVQ_kLJxwqhhnK4-Q"
+    },
+    success: function(data) {
+      console.log(data);
+    }
+  });
+};
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+  var content = contentArr.find(function(content) {
+    return changeInfo.url == content.url;
+  });
+  if (content && currentTab === null) {
+    currentTab = tabId;
+    currentContent = content;
+    timer.start();
+    console.log('URL exists, applicable content');
+  } else {
+    console.log('doesnt');
+  }
+})
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   if (message.command == "StartTimer") {
-    console.log(contentUrls);
     currentTab = message.tabId;
     timer.start();
   } else if (message.command == "StopTimer") {
+    currentTab = null;
     timer.stop();
     timer.reset();
   }
@@ -32,6 +61,13 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 
 chrome.tabs.onRemoved.addListener(function(tabId, b) {
   if (currentTab == tabId) {
+    postContent({
+      "content": currentContent.id,
+      "time": (timer.lap() / 1000) / 60 ,
+      "completed": false
+    });
+    currentTab = null;
+    currentContent = null;
     timer.stop();
     timer.reset();
   }
--- popup.js
+++ popup.js
@@ -9,6 +9,29 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 
 document.addEventListener('DOMContentLoaded', () => {  
   var currentTabUrl;
+
+  $('.login-form').submit(function(e) {
+    e.preventDefault();
+    var loginInfo = {
+      "username": $('#username').val(),
+      "password": $('#password').val()
+    };
+
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:3001/auth/login",
+      datatype: 'json',
+      data: JSON.stringify(loginInfo),
+      headers: {
+        'content-type': 'application/json'
+      },
+      success: function(data) {
+        console.log(data);
+      }
+    });
+  })
+
+  // Timer & tab logic
   chrome.tabs.getSelected(null, function(tab) {
     currentTab = tab;
   });
